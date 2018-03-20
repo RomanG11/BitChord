dc474716e9f176ed75e2c882cbd5e1af7e312c02
Roman <33402067+RomanG11@users.noreply.github.com>
2018-03-20 17:18:32 +0200

    Add files via upload

--- BitChordTruffle/migrations/2_contracts_migration.js
+++ BitChordTruffle/migrations/2_contracts_migration.js
@@ -1,9 +1,11 @@
-var Token = artifacts.require("./DatareumToken.sol");
-var Crowdsale = artifacts.require("./DatareumCrowdsale.sol");
+var Crowdsale = artifacts.require("./BitChordCrowdsale.sol");
+var Token = artifacts.require("./TokenERC20.sol")
+// Token = "0xE7207901406978FC6e443C057A316c103495C474";
 
-// var address = web3.eth.accounts[0];
+var address = web3.eth.accounts[0];
 module.exports = function(deployer) {
-  deployer.deploy(Token,"0xBBBBaAeDaa53EACF57213b95cc023f668eDbA361","0xBBBBaAeDaa53EACF57213b95cc023f668eDbA361").then(function(){
-  	return deployer.deploy(Crowdsale,Token.address);
+  deployer.deploy(Token,1000000000000000000,"TEST","TST").then(function(){
+  	return deployer.deploy(Crowdsale,Token.address,address);
+
   });
 }
